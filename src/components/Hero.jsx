6f10a762eae6d10102ef65441fbcc961ef2b57c9
shzn-mhd <mohamadshazan221@gmail.com>
2024-02-11 17:24:06 +0530

    completing hero section and starting analytics component part

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,10 +7,18 @@ const Hero = () => {
             <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
                 <p className="text-[#00df9a] font-bold p-2">GROWING WITH DATA ANALYTICS</p>
                 <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py6">Grow With Data</h1>
-                <div>
-                    <p className="md:text-5xl sm:text-4xl text-xl font-bold">Fast, Felxible Financing  for</p>
-                    <ReactTyped strings={['BTB' ,'BTC', 'SAAS']} typeSpeed={120} backSpeed={140} loop />
+                <div className="flex justify-center items-center">
+                    <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">
+                        Fast, Felxible Financing  for
+                    </p>
+                    <ReactTyped className="md:text-5xl sm:text-4xl text-xl font-bold pl-2"
+                    strings={['BTB' ,'BTC', 'SAAS']} 
+                    typeSpeed={120} 
+                    backSpeed={140} 
+                    loop />
                 </div>
+                <p className="md:text-2xl text-xl font-bold text-gray-500">Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms</p>
+                <button className="bg-[#00df9a] w-[200px] rounded-md font font-medium my-6 mx-auto py-3 text-black">Get Started</button>
             </div>
         </div>
     )
