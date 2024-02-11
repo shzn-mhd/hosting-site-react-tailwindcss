c93a7e7b0bd79d33c07d7fd7ce4ce38fd62ef047
shzn-mhd <mohamadshazan221@gmail.com>
2024-02-11 20:01:36 +0530

    completed cards components

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -14,21 +14,24 @@ const Cards = () =>{
                         <p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
                         <p className="py-2 border-b mx-8">1 Granted User</p>
                         <p className="py-2 border-b mx-8">Send Up to 2GB</p>
+
+                        <button className=" bg-[#00df9a] w-[200px] rounded-md font font-medium my-6 mx-auto md:mx-0 py-3 text-black"> Start Tial</button>
                     </div>
-                    <button className=" bg-[#00df9a] w-[200px] rounded-md font font-medium my-6 mx-auto md:mx-0 py-3 text-black"> Start Tial</button>
+                    
                 </div>
-                <div className="w-full shadow-xl flex flex-col p-4 my-8 rounded-lg hover:scale-105 duration-300">
-                    <img className="w-20 mx-auto mt-[-3rem] bg-white" src={userTwo} alt="userone" />
+                <div className="w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300">
+                    <img className="w-20 mx-auto mt-[-3rem] bg-transparent" src={userTwo} alt="userone" />
                     <h2 className="text-2xl font-bold text-center py-8">Single User</h2>
                     <p className="text-center text-4xl font-bold">$299</p>
                     <div className="text-center font-medium">
                         <p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
                         <p className="py-2 border-b mx-8">1 Granted User</p>
                         <p className="py-2 border-b mx-8">Send Up to 2GB</p>
+                        <button className="bg-black w-[200px] rounded-md font font-medium my-6 mx-auto md:mx-0 py-3 text-[#00df9a]"> Start Tial</button>
                     </div>
-                    <button className="bg-black w-[200px] rounded-md font font-medium my-6 mx-auto md:mx-0 py-3 text-[#00df9a]"> Start Tial</button>
+                    
                 </div>
-                <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+                <div className="w-full shadow-xl  flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
                     <img className="w-20 mx-auto mt-[-3rem] bg-white" src={userThree} alt="userone" />
                     <h2 className="text-2xl font-bold text-center py-8">Single User</h2>
                     <p className="text-center text-4xl font-bold">$299</p>
@@ -36,8 +39,9 @@ const Cards = () =>{
                         <p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
                         <p className="py-2 border-b mx-8">1 Granted User</p>
                         <p className="py-2 border-b mx-8">Send Up to 2GB</p>
+                        <button className="bg-[#00df9a] w-[200px] rounded-md font font-medium my-6 mx-auto md:mx-0 py-3 text-black"> Start Tial</button>
                     </div>
-                    <button className="bg-[#00df9a] w-[200px] rounded-md font font-medium my-6 mx-auto md:mx-0 py-3 text-black"> Start Tial</button>
+                   
                 </div>
             </div>
         </div>
