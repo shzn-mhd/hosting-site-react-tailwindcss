e536b3f79cc6658f68ca0a0ae0d2e881da704a3f
shzn-mhd <mohamadshazan221@gmail.com>
2024-02-11 17:41:21 +0530

    import images for the analytics components and designing the skeleton for analytics part

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import laptop from '../assets/laptop.png';
 
 const Analytics = () =>{
     return(
-        <div>Analytics</div>
+        <div className="w-full bg-white py-16 px-4">
+            <div className="max-w-[1240px] mx-auto grid md:grid-cols-2"></div>
+                <img src={laptop} alt="Laptop" /> 
+                <div>
+                    <p>DATA ANALYTICS DASHBOARD</p>
+                    <h1>Manage Data Analytics Centrally</h1>
+                    
+                </div>
+        </div>
     )
 }
 
