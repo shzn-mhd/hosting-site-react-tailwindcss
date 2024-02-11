957747d42b48612003ab362e7b474a65d65d9ccc
shzn-mhd <mohamadshazan221@gmail.com>
2024-02-11 17:04:51 +0530

    creating hero component and some styles for the font and install typed npm module for animation

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+    return (
+        <div className="text-white">
+            <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+                <p className="text-[#00df9a] font-bold p-2">GROWING WITH DATA ANALYTICS</p>
+                <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py6">Grow With Data</h1>
+                <div>
+                    <p className="md:text-5xl sm:text-4xl text-xl font-bold">Fast, Felxible Financing  for</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
