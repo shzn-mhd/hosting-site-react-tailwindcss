ec1d95b33e9e48542ace12d9682f18838623273f
shzn-mhd <mohamadshazan221@gmail.com>
2024-02-11 18:03:10 +0530

    completing the analytics conatiner and creating newlsetter container skeleton

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -4,13 +4,19 @@ import laptop from '../assets/laptop.png';
 const Analytics = () =>{
     return(
         <div className="w-full bg-white py-16 px-4">
-            <div className="max-w-[1240px] mx-auto grid md:grid-cols-2"></div>
-                <img src={laptop} alt="Laptop" /> 
-                <div>
-                    <p>DATA ANALYTICS DASHBOARD</p>
-                    <h1>Manage Data Analytics Centrally</h1>
-                    
+            <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+                <img className="w-[500px] mx-auto my-4" src={laptop} alt="Laptop" /> 
+                <div className="flex flex-col justify-center">
+                    <p className="text-[#00df9a] font-bold">DATA ANALYTICS DASHBOARD</p>
+                    <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">Manage Data Analytics Centrally</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Soluta ad optio commodi iure veritatis rem numquam et. 
+                        Quam illum id eaque perferendis sed dolore asperiores praesentium, mollitia, corporis dolorem quasi!
+                    </p>
+                    <button className="bg-black w-[200px] rounded-md font font-medium my-6 mx-auto md:mx-0 py-3 text-[#00df9a]">Get Started</button>
                 </div>
+            </div>
         </div>
     )
 }
---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NewsLetter = () => {
+    return (
+        <div className="w-full py-16 text-white">
+            <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3"></div>
+
+        </div>
+    )
+}
+
+export default NewsLetter
